d7a8ab9e0cbda40af0d23cfd2862edcc028709b9
bettercallpat <squashpat89@googlemail.com>
2016-04-09 02:22:36 +0200

    Express app up and running

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 var express = require('express');
-
 var app = express();
 
-var port = 5000;
+app.use(express.static('public'));
+app.use(express.static('src/views'));
 
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
+var SerialPort = require("serialport").SerialPort;
+var serialport = new SerialPort("/dev/cu.usbmodem1411");
+
+var streamObj = {};
+serialport.on('data', function(data) {
+    // console.log('data received: ' + data);
+    streamObj = data;
+});
+
+app.get('/', function(req, res) {
+    res.render('index', {title: 'hello world', number2: 334});
+});
+
+var port = 5000;
 app.listen(port, function(err) {
     console.log('Running on port ' + port);
 });
